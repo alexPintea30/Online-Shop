7791168ae564c80fd8cc8de33ed9d155ac4512d9
melinda.kotai <melindakotai@gmail.com>
2020-07-28 12:21:56 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'BookController@index');
+Route::get('/book/{book}','BookController@show');
+
+Route::get("/testDB", function(){
+
+});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController.php@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController.php@index')->name('home');
+
+Route::group(['prefix' => 'cost'], function(){
+    /*
+     * Pentru a primi costul unei carti in functie de judet, varsta si categorie
+     */
+    Route::get('/getCost/{basePrice}/{region}/{age}/{category}', function($basePrice, $region, $age, $category){
+       $controller = new CostController;
+       return $controller->cost($basePrice, $region, $age, $category);
+    });
+    Route::get('/version', "VersionController@index");
+});
+
+Route::get('/home/{id}', function($id){
+   return "User:".$id;
+});
